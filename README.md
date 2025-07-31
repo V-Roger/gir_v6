fa600cecfccf5b96f020fe7a3bacb97c248cb762
Virgil Roger <roger.virgil@gmail.com>
2025-07-31 08:51:21 +0200

    🎉 initialize sveltekit app

---
+++ README.md
@@ -0,0 +1,4 @@
+# virgil-roger.photography
+
+2025 edition, now with Svelte
+_(because there's always a new JS framework to try out, right?)_
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import '../app.css';
+
+	let { children } = $props();
+</script>
+
+<aside>
+	<img src="/assets/logo_vroger.svg" alt="Logo de Virgil Roger" width="100" />
+	<nav>
+		<a href="/home">Home</a>
+		<a href="/bio">Bio</a>
+	</nav>
+</aside>
+
+<section>
+	{@render children()}
+</section>
