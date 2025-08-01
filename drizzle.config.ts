1039b20ae1679b79db2cac86d95a95d4128cc33f
Virgil Roger <roger.virgil@gmail.com>
2025-08-01 15:18:06 +0200

    🔧 allow db url to be set by netlify env var

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'drizzle-kit';
 
-if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+const dbUrl = process.env.NETLIFY_DATABASE_URL ?? process.env.DATABASE_URL;
+
+if (!dbUrl) throw new Error('no DATABASE_URL nor NETLIFY_DATABASE_URL set');
 
 export default defineConfig({
 	schema: './src/lib/server/db/schema.ts',
 	dialect: 'postgresql',
-	dbCredentials: { url: process.env.DATABASE_URL },
+	dbCredentials: { url: dbUrl },
 	verbose: true,
 	strict: true
 });
