9d7ff913c6032e239dcff7bbcf879b505775c998
Virgil Roger <roger.virgil@gmail.com>
2025-08-01 17:52:05 +0200

    🐛 fix slug reactivity

--- src/routes/photos/[slug]/+page.server.ts
+++ src/routes/photos/[slug]/+page.server.ts
@@ -3,12 +3,13 @@ import { eq, inArray } from 'drizzle-orm';
 import { db } from '$lib/server/db';
 import type { PageServerLoad } from './$types';
 
-
-export const load: PageServerLoad = async ({ params }: { params: { slug: string } }) => {
-	const galleryName = params.slug;
-
-	const gallery = await db.select().from(galleries).where(eq(galleries.slug, galleryName)).limit(1);
+async function fetchGallery(slug: string) {
+	const gallery = await db.select().from(galleries).where(eq(galleries.slug, slug)).limit(1);
 	const galleryPhotos = await db.select().from(photos).where(inArray(photos.id, gallery?.[0]?.photos ?? []));
 
 	return { gallery: gallery?.[0], photos: galleryPhotos.map(p => ({ path: p.path, description: p.description })) ?? [] };
+};
+
+export const load: PageServerLoad = async ({ params }: { params: { slug: string } }) => {
+	return await fetchGallery(params.slug);
 };
--- src/routes/photos/[slug]/+page.svelte
+++ src/routes/photos/[slug]/+page.svelte
@@ -3,19 +3,18 @@
 	import type { PageProps } from './$types';
 
 	const { data }: PageProps = $props();
-	const { gallery, photos } = data;
 
-	if (!gallery || !photos) {
+	if (!data?.gallery || !data?.photos) {
 		throw redirect(302, '/photos');
 	}
 </script>
 
-<h1>{gallery.name}</h1>
-<p>{gallery.description}</p>
+<h1>{data.gallery.name}</h1>
+<p>{data.gallery.description}</p>
 
 <div class="flex flex-col items-end not-prose gap-4">
-	{#if photos}
-		{#each photos as photo}
+	{#if data.photos}
+		{#each data.photos as photo}
 			<img src={`/.netlify/images?url=${photo.path}`} alt={ photo.description ?? 'missing description' } class="object-contain max-w-full max-h-screen" />
 		{/each}
 	{:else}
