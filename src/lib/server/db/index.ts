f2eea64d72768b0b138d840b3c4defd705a34e4f
Virgil Roger <roger.virgil@gmail.com>
2025-08-01 15:21:32 +0200

    💩 add forgotten file

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -3,8 +3,10 @@ import { neon } from '@neondatabase/serverless';
 import * as schema from './schema';
 import { env } from '$env/dynamic/private';
 
-if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+const dbUrl = env.NETLIFY_DATABASE_URL ?? env.DATABASE_URL;
 
-const client = neon(env.DATABASE_URL);
+if (!dbUrl) throw new Error('no DATABASE_URL nor NETLIFY_DATABASE_URL set');
+
+const client = neon(dbUrl);
 
 export const db = drizzle(client, { schema });
