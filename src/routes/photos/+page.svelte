cb2f76e16a12bb69c8dd760c70aa15061ad48b27
Virgil Roger <roger.virgil@gmail.com>
2025-08-02 10:44:20 +0200

    🐛 fix padding on ul

--- src/routes/photos/+page.svelte
+++ src/routes/photos/+page.svelte
@@ -2,7 +2,7 @@
   let { data } = $props()
 </script>
 
-<ul class="grid list-none grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+<ul class="grid list-none grid-cols-1 gap-4 pl-0! md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
   {#each data.galleries as gallery}
     <li class="flex flex-col items-start justify-start rounded-lg bg-black p-2 dark:bg-white">
       <a href={`/photos/${gallery.slug}`}>
