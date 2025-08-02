63cde7e976acb17442787f80fa3b4246bead954e
Virgil Roger <roger.virgil@gmail.com>
2025-08-02 10:39:12 +0200

    ✨ add photos galleries grid

---
+++ src/routes/photos/+page.server.ts
@@ -0,0 +1,31 @@
+import { db } from '$lib/server/db'
+import { galleries, photos } from '$lib/server/db/schema'
+import { eq } from 'drizzle-orm'
+import type { PageServerLoad } from './$types'
+
+async function hydrateCoverPhotos(galleriesList: (typeof galleries.$inferSelect & { photos: number[] | null })[]) {
+  return Promise.all(
+    galleriesList.map(async (gallery) => {
+      if (!gallery?.photos?.length) {
+        return { ...gallery, cover: undefined }
+      }
+
+      const cover = await db.select().from(photos).where(eq(photos.id, gallery.photos[0]))
+
+      return { ...gallery, cover: cover?.[0] ?? undefined }
+    }),
+  )
+}
+
+async function fetchGalleriesWithCover() {
+  const galleriesList = await db.select().from(galleries)
+  if (galleriesList.length === 0) {
+    return []
+  }
+
+  return await hydrateCoverPhotos(galleriesList)
+}
+
+export const load: PageServerLoad = async () => {
+  return { galleries: await fetchGalleriesWithCover() }
+}
---
+++ src/routes/photos/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  let { data } = $props()
+</script>
+
+<ul class="grid list-none grid-cols-1 gap-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+  {#each data.galleries as gallery}
+    <li>
+      <a href={`/photos/${gallery.slug}`}>
+        {#if gallery?.cover?.path}
+          <img
+            src={gallery.cover.path}
+            alt={gallery.name}
+            class="aspect-square w-full rounded-lg object-cover transition-all duration-120 hover:opacity-70"
+          />
+        {/if}
+        <p class="mt-2 text-center">{gallery.name}</p>
+      </a>
+    </li>
+  {/each}
+</ul>
